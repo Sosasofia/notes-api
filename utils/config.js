4b666792acf0b36ebe3284dc9e3ba88f99143e5f
Sosasofia <80438887+Sosasofia@users.noreply.github.com>
2022-09-27 19:34:49 -0300

    Update configuration

--- utils/config.js
+++ utils/config.js
@@ -2,12 +2,16 @@ require("dotenv").config();
 
 const PORT = process.env.PORT || 3001;
 
+let MONGODB_URI;
 
-const MONGODB_URI =
-  process.env.NODE_ENV === "test"
-    ? process.env.TEST
-    : process.env.MONGODB_URI;
-    
+if(process.env.NODE_ENV === "test") {
+  MONGODB_URI = process.env.TEST;
+} else if(process.env.NODE_ENV === "development") {
+  MONGODB_URI = process.env.TEST_MONGODB_URI;
+} else {
+  process.env.MONGODB_URI;
+}
+
 module.exports = {
   MONGODB_URI,
   PORT,
