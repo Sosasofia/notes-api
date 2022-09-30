c5682d0016756d9bfc16bb0e0b80c5b0fc57248d
Sosasofia <80438887+Sosasofia@users.noreply.github.com>
2022-09-29 21:44:33 -0300

    Fix db config

--- utils/config.js
+++ utils/config.js
@@ -8,8 +8,8 @@ if(process.env.NODE_ENV === "test") {
   MONGODB_URI = process.env.TEST;
 } else if(process.env.NODE_ENV === "development") {
   MONGODB_URI = process.env.TEST_MONGODB_URI;
-} else {
-  process.env.MONGODB_URI;
+} else if(process.env.NODE_ENV === "production") {
+  MONGODB_URI = process.env.MONGODB_URI;
 }
 
 module.exports = {
